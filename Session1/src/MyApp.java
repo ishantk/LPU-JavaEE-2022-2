fbe6ec975812756364936a7eaf2778757be8f5db
ishantk <er.ishant@gmail.com>
2022-01-19 17:45:02 +0530

    Session1

---
+++ Session1/src/MyApp.java
@@ -0,0 +1,16 @@
+/**
+ * @author ishant
+ *
+ */
+public class MyApp {
+
+	/**
+	 * @param args: it is command line arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("Thank You all for being a wonderful audience");
+		System.out.println("I will see ya tomorrow now");
+		System.out.println("Code the same what we did in the class");
+	}
+
+}
